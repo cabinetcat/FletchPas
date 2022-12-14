7cc1b0c243b46552fa5c166c5f8aa721a1f22681
Hunter Wheat <elitewheatproductionz@gmail.com>
2022-12-13 23:08:23 -0800

    Actually works now

--- FletchPas.pas
+++ FletchPas.pas
@@ -1,44 +1,58 @@
 unit FletchPas;
+
+{$mode ObjFPC}{$H+}
+
 interface
-uses classes;
-    function Fletcher16(Stream: TStream): word;
-    function Fletcher32(Stream: TStream): longword;
-    function Fletcher64(Stream: TStream): qword;
+         function Fletcher16(B: array of Byte): Word;
+         function Fletcher32(B: array of Byte): LongWord;
+         function Fletcher64(B: array of Byte): QWord;
+
+
 implementation
-    function Fletcher16(Stream: TStream; startIndex: qword = 0; endIndex: qword = nil): word;
-    var
-        a,b: byte;
-        endReadIndex: qword;
-        Buffer: array[0.. Stream.Size - 1] of byte;
-    begin        
-        if endIndex = nil then endReadIndex:= Stream.Size;
-        Stream.Seek(startIndex,0);
-        Stream.ReadBuffer(Buffer, endReadIndex);
-        for _b in Buffer do
-            begin
-                a:= (a + _b) mod $ff;
-                b:= (b + a) mod $ff;
-            end;  
-        Fletcher16:= (b << 8) or a;
-    end;
-    function Fletcher32(Stream: TStream; startIndex: qword = 0; endIndex: qword = nil): longword;
-    var
-        a,b: word;
-        endReadIndex: qword;
-        Buffer: array[0.. Stream.Size - 1] of byte;
-    begin        
-        if endIndex = nil then endReadIndex:= Stream.Size;
-        Stream.Seek(startIndex,0);
-        Stream.ReadBuffer(Buffer, endReadIndex);
-        for _b in Buffer do
-            begin
-                a:= (a + _b) mod $ffff;
-                b:= (b + a) mod $ffff;
-            end;  
-        Fletcher32:= (b << 16) or a;
-    end;
+              function Fletcher16(B: array of Byte): Word;
+              var
+                 sum1, sum2: Byte;
+                 i: QWord;
+              begin
+                   sum1 := 0;
+                   sum2 := 0;
+                   for i := 0 to length(B)-1 do
+                       begin
+	               sum1 += B[i] mod 255;
+                       sum2 += sum1 mod 255;
+                       end;
 
+                   Result := (Word(sum1) shl 8) or sum2;
+              end;
+              function Fletcher32(B: array of Byte): LongWord;
+              var
+                 sum1, sum2: Word;
+                 i: QWord;
+              begin
+                   sum1 := 0;
+                   sum2 := 0;
+                   for i := 0 to length(B)-1 do
+                       begin
+	               sum1 += B[i] mod 65535;
+                       sum2 += sum1 mod 65535;
+                       end;
 
-    
-    
+                   Result := (LongWord(sum1) shl 16) or sum2;
+              end;
+              function Fletcher64(B: array of Byte): QWord;
+              var
+                 sum1, sum2: LongWord;
+                 i: QWord;
+              begin
+                   sum1 := 0;
+                   sum2 := 0;
+                   for i := 0 to length(B)-1 do
+                       begin
+	               sum1 += B[i] mod 4294967295;
+                       sum2 += sum1 mod 4294967295;
+                       end;
+
+                   Result := (QWord(sum1) shl 32) or sum2;
+              end;
+
 end.
